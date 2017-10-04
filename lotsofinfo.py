4cb05b0b43cb04060bf17863bd25725201a218ac
David An <andavid94@gmail.com>
2017-10-03 21:26:19 -0700

    Update incorporating pictures

--- lotsofinfo.py
+++ lotsofinfo.py
@@ -28,14 +28,15 @@ session.add(User1)
 session.commit()
 
 
-# Create category for tennis players
-Category1 = Category(name = "Tennis Players", user_id = 1)
+# Create category for areas in the Pacific Northwest
+Category1 = Category(name = "Pacific Northwest", user_id = 1)
 session.add(Category1)
 session.commit()
 
 Item1 = Items(
-		name = "Roger Federer",
-		description = "Arguably the greatest tennis player of all time.",
+		name = "Seattle, WA",
+		picture = "https://cdn.vox-cdn.com/uploads/chorus_image/image/53737065/eatersea0317_seattle_shutterstock.0.jpg",
+		description = "Seattle is the largest city in the Pacific Northwest region of the United States. It is located in the U.S. state of Washington, about 108 miles (180 km) south of the American-Canadian border. ... Its official nickname is the Emerald City.",
 		date = datetime.datetime.utcnow(),
 		category_id = 1, 
 		user_id = 1)
@@ -43,8 +44,9 @@ session.add(Item1)
 session.commit()
 
 Item2 = Items(
-		name = "Rafael Nadal",
-		description = "World renowned spanish player",
+		name = "Portland, OR",
+		picture = "https://www.visittheusa.com/sites/default/files/styles/hero_m_1300x700/public/images/hero_media_image/2016-11/Drone.__72%20DPI.jpg?itok=jhGikGPz",
+		description = "This climate is ideal for growing roses, and Portland has been called the City of Roses for over a century. Keep Portland Weird is an unofficial slogan for the city.",
 		date = datetime.datetime.utcnow(),
 		category_id = 1,
 		user_id = 1)
@@ -52,14 +54,15 @@ session.add(Item2)
 session.commit()
 
 
-# Create category for foods
-Category2 = Category(name = "Foods", user_id = 1)
+# Create category for areas in California
+Category2 = Category(name = "Southwest", user_id = 1)
 session.add(Category2)
 session.commit()
 
 Item3 = Items(
-		name = "Sushi",
-		description = "Raw fish paired with slightly salted rice",
+		name = "San Diego",
+		picture = "http://www.hotelpalomar-sandiego.com/images/tout/kimpton-hotel-palomar-san-diego-pier-b6a08a84.jpg",
+		description = "Beautiful weather year-round, and an excellent place to vacation if you are a surfer or enjoy eating good mexican food.",
 		date = datetime.datetime.utcnow(),
 		category_id = 2,
 		user_id = 1)
@@ -68,43 +71,27 @@ session.commit()
 
 
 # Create category for travel destinations
-Category3 = Category(name = "Destinations", user_id = 1)
+Category3 = Category(name = "Midwest", user_id = 1)
 session.add(Category3)
-session.commit()
-
-Item4 = Items(
-		name = "New Zealand",
-		description = "Filming location for the Lord of the Rings",
-		date = datetime.datetime.utcnow(),
-		category_id = 3,
-		user_id = 1)
-session.add(Item4)
 session.commit()
 
 
 # Create category for musical artists
-Category4 = Category(name = "Musical Artists", user_id = 1)
+Category4 = Category(name = "Hawaiian islands", user_id = 1)
 session.add(Category4)
 session.commit()
 
-Item5 = Items(
-		name = "Coldplay",
-		description = "Soft rock band",
-		date = datetime.datetime.utcnow(),
-		category_id = 4,
-		user_id = 1)
-session.add(Item5)
+Category5 = Category(name = "Northeast", user_id = 1)
+session.add(Category5)
+session.commit()
+
+Category6 = Category(name = "Southeast", user_id = 1)
+session.add(Category6)
+session.commit()
+
+Category7 = Category(name = "Alaska", user_id = 1)
+session.add(Category7)
 session.commit()
 
 
 print "added category and item information to database"
-
-
-
-
-
-
-
-
-
-

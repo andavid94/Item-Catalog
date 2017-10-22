4009694c4cf2131fc76552caf93dad2fa18f3da5
David An <andavid94@gmail.com>
2017-10-22 15:29:54 -0700

    Update application.py
    Adapt code to accept regions and cities instead of categories and items

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import Flask, jsonify, flash, session as login_session
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Items, User
+from database_setup import Base, Region, Cities, User
 import random, string, os, datetime
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
@@ -20,7 +20,7 @@ CLIENT_ID = json.loads(
 APPLICATION_NAME = "Item Catalog Application"
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///itemcatalog.db')
+engine = create_engine('sqlite:///travelDocument.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
@@ -63,10 +63,6 @@ def gconnect():
            % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    #response = h.request(url, 'GET')[1]
-    #str_response = response.decode('utf-8')
-    #result = json.loads(str_response)
-    # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
@@ -181,38 +177,26 @@ def getUserID(email):
 
 # JSON APIs to view Catalog information
 @app.route('/catalog/JSON')
-def allItemsJSON():
-	categories = session.query(Category).all()
-	category_dict = [c.serialize for c in categories]
-	for c in range(len(category_dict)):
-		items = [i.serialize for i in session.query(Items)
-				.filter_by(category_id = category_dict[c]['id']).all()]
-		if items:
-			category_dict[c]['Item'] = items
-	return jsonify(Category = category_dict)
+@app.route('/catalog/regions/JSON')
+def showCatalogJSON():
+	regions = session.query(Region).all()
+	return jsonify(regions = [region.serialize for region in regions])
 
-@app.route('/catalog/categories/JSON')
-def categoriesJSON():
-	categories = session.query(Category).all()
-	return jsonify(categories = [c.serialize for c in categories])
+@app.route('/catalog/cities/JSON')
+def citiesJson():
+	cities = session.query(Cities).all()
+	return jsonify(cities = [city.serialize for city in cities])
 
-@app.route('/catalog/items/JSON')
-def itemsJson():
-	items = session.query(Items).all()
-	return jsonify(items = [i.serialize for i in items])
+@app.route('/catalog/<path:region_name>/cities/JSON')
+def regionCiiesJSON(region_name):
+	region = session.query(Region).filter_by(name = region_name).one()
+	cities = session.query(Cities).filter_by(region = region_name).all()
+	return jsonify(cities = [city.serialize for city in cities])
 
-@app.route('/catalog/<path:category_name>/items/JSON')
-def categoryItemsJSON(category_name):
-	category = session.query(Category).filter_by(name = category_name).one()
-	items = session.query(Items).filter_by(category = category).all()
-	return jsonify(items = [i.serialize for i in items])
-
-@app.route('/catalog/<path:category_name>/<pathLitem_name>/JSON')
-def ItemJSON(category_name, item_name):
-	category = session.query(Category).filter_by(name = category_name).one()
-	item = session.query(Items).filter_by(name = item_name,
-										category = category).one()
-	return jsonify(item = [item.serialize])
+@app.route('/catalog/<path:region_name>/<path:city_name>/JSON')
+def CityJSON(region_name, city_name):
+	city = session.query(Cities).filter_by(name = city_name).first()
+	return jsonify(city = [city.serialize])
 
 
 # Flask routing
@@ -220,130 +204,114 @@ def ItemJSON(category_name, item_name):
 @app.route('/')
 @app.route('/catalog/')
 def showCatalog():
-	categories = session.query(Category).order_by(asc(Category.name))
-	items = session.query(Items).order_by(asc(Items.date))
+	regions = session.query(Region).order_by(asc(Region.name))
+	cities = session.query(Cities).order_by(desc(Cities.date))
 	return render_template(
-		'catalog.html', categories = categories, items = items)
+		'catalog.html', regions = regions, cities = cities)
 
 
 
-# Show items in given category
-@app.route('/catalog/<path:category_name>/items/')
-def showCategory(category_name):
-	categories = session.query(Category).order_by(asc(Category.name))
-	category = session.query(Category).filter_by(name = category_name).one()
-	creator = getUserInfo(category.user_id)
-	items = session.query(Items).filter_by(
-			category = category).all()
-	print items
-	#count = session.query(Items).filter_by(category = category).count()
+# Show cities in given region
+@app.route('/catalog/<path:region_name>/cities/')
+def showRegion(region_name):
+	regions = session.query(Region).order_by(asc(Region.name))
+	region = session.query(Region).filter_by(name = region_name).one()
+	creator = getUserInfo(region.user_id)
+	cities = session.query(Cities).filter_by(
+			region = region).all()
+	print cities
 	if 'username' not in login_session or creator.id != login_session.get('user_id'):
-		return render_template('publicItems.html', category = category.name,
-								categories = categories, items = items)
+		return render_template('publicCities.html', region = region.name,
+								regions = regions, cities = cities)
 	else:
 		user = getUserInfo(login_session['user_id'])
-		return render_template('items.html', category = category.name,
-								categories = categories, items = items, user = user)
+		return render_template('cities.html', region = region.name,
+								regions = regions, cities = cities, user = user)
 
 
-# Show a particular item
-@app.route('/catalog/<path:category_name>/<path:item_name>/')
-def showItem(category_name, item_name):
-	categories = session.query(Category).order_by(asc(Category.name))
-	item = session.query(Items).filter_by(name = item_name).one()
-	creator = getUserInfo(item.user_id)
+# Show a particular city
+@app.route('/catalog/<path:region_name>/<path:city_name>/')
+def showCity(region_name, city_name):
+	regions = session.query(Region).order_by(asc(Region.name))
+	city = session.query(Cities).filter_by(name = city_name).one()
+	creator = getUserInfo(city.user_id)
 	if 'username' not in login_session or creator.id != login_session.get('user_id'):
-		return render_template('publicitemdetail.html', category = category_name,
-								categories = categories, item = item)
+		return render_template('publiccitydetail.html', region = region_name,
+								regions = regions, city = city)
 	else:
-		return render_template('itemdetail.html', category = category_name,
-								categories = categories, item = item, creator = creator)
+		return render_template('citydetail.html', region = region_name,
+								regions = regions, city = city, creator = creator)
 
 
-# Create a new Item
+# Create a new City
 @app.route('/catalog/add/', methods = ['GET', 'POST'])
-def addItem():
-	categories = session.query(Category).all()
+def addCity():
+	regions = session.query(Region).all()
 	if request.method == 'POST':
-		newItem = Items(name = request.form['name'], picture = request.form['picture'],
-			description = request.form['description'], date = datetime.datetime.now(), 
-			category = session.query(Category).
-			filter_by(name = request.form['category']).one(), user_id = login_session.get('user_id'))
-		session.add(newItem)
+		newCity = Cities(name = request.form['name'], picture = request.form['picture'],
+			description = request.form['description'], date = datetime.datetime.utcnow(), 
+			region = session.query(Region).
+			filter_by(name = request.form['region']).one(), user_id = login_session.get('user_id'))
+		session.add(newCity)
 		session.commit()
-		flash('New Item has successfully been created')
+		flash('New City has successfully been created')
 		return redirect(url_for('showCatalog'))
 	else: 
-		return render_template('addItem.html', categories = categories)
+		return render_template('addCity.html', regions = regions)
 
 
-# Edit an existing Item
-@app.route('/catalog/<path:category_name>/<path:item_name>/edit', methods=['GET', 'POST'])
-def editItem(category_name, item_name):
-	categories = session.query(Category).all()
-	editedItem = session.query(Items).filter_by(name = item_name).one()
-	creator = getUserInfo(editedItem.user_id)
+# Edit an existing City
+@app.route('/catalog/<path:region_name>/<path:city_name>/edit', methods=['GET', 'POST'])
+def editCity(region_name, city_name):
+	regions = session.query(Region).all()
+	editedCity = session.query(Cities).filter_by(name = city_name).one()
+	creator = getUserInfo(editedCity.user_id)
 	user = getUserInfo(login_session.get('user_id'))
 	if login_session.get('user_id') != creator.id:
-		flash('You cannot edit this item since you are not the creator')
+		flash('You cannot edit this city since you are not the creator')
 		return redirect(url_for('showCatalog'))
 	if request.method == 'POST':
 		if request.form['name']:
-			editedItem.name = request.form['name']
+			editedCity.name = request.form['name']
 		if request.form['picture']:
-			editedItem.picture = request.form['picture']
+			editedCity.picture = request.form['picture']
 		if request.form['description']:
-			editedItem.description = request.form['description']
-		if request.form['category']:
-			category = session.query(Category).filter_by(name = 
-					request.form['category']).one()
-			editedItem.category = category 
+			editedCity.description = request.form['description']
+		if request.form['region']:
+			region = session.query(Region).filter_by(name = 
+					request.form['region']).one()
+			editedCity.region = region 
 		time = datetime.datetime.utcnow()
-		editedItem.date = time
-		session.add(editedItem)
+		editedCity.date = time
+		session.add(editedCity)
 		session.commit()
-		flash('Item has been successfully edited.')
-		return redirect(url_for('showCategory', 
-				category_name = editedItem.category.name))
+		flash('Your city has been successfully edited.')
+		return redirect(url_for('showRegion', 
+				region_name = editedCity.region.name))
 	else:
-		return render_template('edititem.html', item = editedItem,
-								categories = categories)
+		return render_template('editcity.html', city = editedCity,
+								regions = regions)
 
 
-# Delete an existing Item
-@app.route('/catalog/<path:category_name>/<path:item_name>/delete', methods=['GET', 'POST'])
-def deleteItem(category_name, item_name):
-	itemToDelete = session.query(Items).filter_by(name = item_name).one()
-	categories = session.query(Category).all()
-	category = session.query(Category).filter_by(name = category_name).one()
-	creator = getUserInfo(itemToDelete.user_id)
+# Delete an existing City
+@app.route('/catalog/<path:region_name>/<path:city_name>/delete', methods=['GET', 'POST'])
+def deleteCity(region_name, city_name):
+	cityToDelete = session.query(Cities).filter_by(name = city_name).one()
+	regions = session.query(Region).all()
+	region = session.query(Region).filter_by(name = region_name).one()
+	creator = getUserInfo(cityToDelete.user_id)
 	user = getUserInfo(login_session.get('user_id'))
 	if login_session.get('user_id') != creator.id:
-		flash('You cannot delete this item since you are not the creator')
+		flash('You cannot delete this city since you are not the creator')
 		return redirect(url_for(showCatalog))
 	if request.method == 'POST':
-		session.delete(itemToDelete)
+		session.delete(cityToDelete)
 		session.commit()
-		flash('Item has been successfully deleted')
-		return redirect(url_for('showCategory', category_name = category.name))
+		flash('Your city has been successfully deleted')
+		return redirect(url_for('showRegion', region_name = region.name))
 	else: 
-		return render_template('deleteitem.html', item = itemToDelete)
+		return render_template('deletecity.html', city = cityToDelete)
 
-
-
-
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
 
 
 
@@ -351,3 +319,4 @@ if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
 	app.debug = True
 	app.run(host = '0.0.0.0', port = 8000)
+
